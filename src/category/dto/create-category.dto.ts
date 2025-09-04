df97097dccf6cac6c89c3664d26410618def6ec0
EAEzequielAlonso <eze.g.alonso@gmail.com>
2025-09-04 11:27:21 -0300

    arreglo itemsCarts

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateCategoryDto {
+    @ApiProperty({ description: 'Nombre de la Categoria' })
     @IsString()
     name: string
 }
--- src/email/dto/create-email.dto.ts
+++ src/email/dto/create-email.dto.ts
@@ -1,6 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString } from "class-validator";
+
 export class CreateEmailDto {
+    @ApiProperty({ description: 'Email del destinatario' })
+    @IsEmail()
     to: string; 
+
+    @ApiProperty({ description: 'Asunto del email' })
+    @IsString()
     subject: string;
+
+    @ApiProperty({ description: 'Texto a incluir en caso de que falle el html, Puede ser la misma informacion del html pero sin estilos' })
+    @IsString()
     text: string; 
+
+    @ApiProperty({ description: 'HTML con la informacion estilizada.' })
+    @IsString()
     html?: string;
 }
