2732bcc787e68148969dae900e5833929cf4040f
EAEzequielAlonso <eze.g.alonso@gmail.com>
2025-10-24 18:31:58 -0300

    agregar socket a userEventPass y agregado a app module

--- src/modules/user-event-pass/user-event-pass.module.ts
+++ src/modules/user-event-pass/user-event-pass.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserEventPassService } from './user-event-pass.service';
 import { UserEventPassController } from './user-event-pass.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEventPass } from './entities/user-event-pass.entity';
+import { UserEventPassRepository } from './user-event-pass.repository';
+import { NotificationsGateway } from '../notification-socket/notification-socket.gateway';
+import { User } from '../users/entities/users.entity';
+import { UsersRepository } from '../users/users.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEventPass, User])],
   controllers: [UserEventPassController],
-  providers: [UserEventPassService],
+  providers: [UserEventPassService, UserEventPassRepository, NotificationsGateway, UsersRepository],
 })
 export class UserEventPassModule {}
