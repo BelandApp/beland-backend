68ede1b89421f2524b3f4d6c4cff278c9eb76d32
EAEzequielAlonso <eze.g.alonso@gmail.com>
2025-12-28 23:01:03 -0300

    sumar servicio cloudinary a services

--- src/modules/services/services.module.ts
+++ src/modules/services/services.module.ts
@@ -4,10 +4,11 @@ import { ServiceController } from './services.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
 import { ServiceRepository } from './services.repository';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Service])],
   controllers: [ServiceController],
-  providers: [ServiceService, ServiceRepository],
+  providers: [ServiceService, ServiceRepository, CloudinaryService],
 })
 export class ServicesModule {}
